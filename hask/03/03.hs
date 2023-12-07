88f4d435d345f16dd6723d0b377e02e45981d056
Simon Millsap <simonmillsap@gmail.com>
2023-12-06 16:19:05 -0800

    thank god

--- hask/03/03.hs
+++ hask/03/03.hs
@@ -2,25 +2,54 @@ import System.IO
 import Control.Monad
 import Data.Char
 import Data.Maybe
-import Data.List
+import Data.List as L
 import Control.Applicative
-import Data.Foldable
+import Data.Foldable as F
 import Data.List.Split
 import Debug.Trace
+import Data.Set as S
 
 main = do
     contents <- readFile "../../input/3.txt" 
 
-    let collections = getCollections $ lines contents
-    let maxY = length $ collections
-    let maxX = length $ head collections
-    let allPoints = concat collections
+    let points = createPoints $ lines contents
 
-    let symbols = filterSymbols allPoints
-    print $ sum $ getValidNumbers maxX maxY symbols allPoints
+    let symbols = filterSymbols points
+    let numPos = getNumberPositions points
 
-surrounds w h (x,y) = filter validPos [l,r,u,d,ul,ur,dl,dr]
-        where validPos (a,b) = a < w && a >= 0 && b < h && b >= 0
+    print $ (sum) $ fst <$> L.filter (nearbySymbol symbols) numPos
+
+    let gears = L.filter (\s -> (thrd s == '*')) symbols
+    let numberCandidates = L.filter (nearbySymbol gears) numPos
+    print $ calcGears numberCandidates gears
+
+calcGears nums [g]    | isPair = (head cands) * (head $ tail cands)
+                      | otherwise = 0
+            where nextTo = S.fromList $ surrounds (frst g,scnd g)
+                  cands = findPair nums nextTo
+                  isPair = length cands == 2
+
+calcGears nums (g:gs) | isPair = (head cands) * (head $ tail cands) + (calcGears nums gs)
+                      | otherwise = calcGears nums gs
+            where nextTo = S.fromList $ surrounds (frst g,scnd g)
+                  cands = findPair nums nextTo
+                  isPair = length cands == 2
+
+findPair [(v,ps)] set    = []
+findPair ((v,ps):ns) set | posIn set ps = v : findSecond ns set
+                         | otherwise = findPair ns set
+
+findSecond [(v,ps)] set | posIn set ps = [v]
+                        | otherwise    = []
+findSecond ((v,ps):ns) set | posIn set ps = v : findSecond ns set
+                           | otherwise    = findSecond ns set
+
+
+posIn set ps = any (`S.member` set) ps
+
+
+surrounds (x,y) = [l,r,u,d,ul,ur,dl,dr]
+            where
               l = (x-1,y)
               r = (x+1,y)
               u = (x,y-1)
@@ -30,41 +59,44 @@ surrounds w h (x,y) = filter validPos [l,r,u,d,ul,ur,dl,dr]
               dl = (x-1,y+1)
               dr = (x+1,y+1)
 
-getNums [(x,y,s)]  = [s]
-getNums ((x,y,s):ns) = s : getNums(ns)
 
-getPos (x,y,s) = (x,y)
+nearbySymbol symbols input = not $ F.null $ L.filter inSymbols $ concat $ surrounds <$> positions 
+            where 
+            positions = snd input
+            inSymbols  = (`elem` (getPos <$> symbols))
+                                
 
-nearbySymbol symbols x y positions = not $ null $ filter (`elem` (getPos <$> symbols)) $ concat $  surrounds x y <$> (positions))
+getNumberPositions []  = []
+getNumberPositions [(x,y,n)] | isDigit n = [(ord n, [(x,y)])]
+                             | otherwise = []
 
+getNumberPositions (n:ns) | isNum n = numPos : (getNumberPositions next)
+                          | otherwise = getNumberPositions ns
+                        where next = dropWhile (isNumAndInLine n) (n:ns)
+                              sepNumPos = takeWhile (isNumAndInLine n) (n:ns)
+                              poss = getPos <$> sepNumPos
+                              num = read $ getNums sepNumPos
+                              numPos = (num,poss)
 
-getValidNumbers xmax ymax symbols [n] | isNum n && (nearbySymbol symbols xmax ymax [getPos n]) = [read $ show $ thrd n]
-                                      | otherwise = []
-getValidNumbers xmax ymax symbols (n:ns) | isNum n && (nearbySymbol symbols xmax ymax positions) = num : (getValidNumbers xmax ymax symbols rest)
-                                         | isNum n = getValidNumbers xmax ymax symbols rest
-                                        
-                                         | otherwise = getValidNumbers xmax ymax symbols ns 
-                                        where 
-                                        collection = takeWhile (isNumAndInLine (frst n)) (n:ns) where
-                                        rest = dropWhile (isNumAndInLine (frst n)) (n:ns)
-                                        positions = getPos <$> collection
-                                        num = read $ getNums collection :: Integer
-getValidNumbers _ _ _ _ = []
+createPoints lines = do
+    (y, row) <- zip [0..] lines
+    (x, value) <- zip [0..] row
+    return (x,y,value)
 
-
-
-getCollections ss = createCollection <$> (zip [0..] ss)
-
-createCollection (n, s) = (\(r,(c,sym)) -> (r,c,sym)) <$> (zip (repeat n) (zip [0..] s))
-
-
-filterSymbols = filter (isValidSymbol . thrd) 
+filterSymbols = L.filter (isValidSymbol . thrd) 
 
 isNum = isDigit . thrd
 
 frst (a,_,_) = a
+scnd (_,b,_) = b
 thrd (_,_,c) = c
 
-isNumAndInLine c (x,y,z) = x==c && isDigit z
+isNumAndInLine n (x,y,z) = y==(scnd n) && isDigit z
 
 isValidSymbol c = not $ isAlphaNum c || c == '.'
+
+getNums []         = []
+getNums [(x,y,s)]  = [s]
+getNums ((x,y,s):ns) = s : getNums(ns)
+
+getPos (x,y,s) = (x,y)
